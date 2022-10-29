fc958d52538d276df5144e65b056f4e561ce5b05
Munif Tanjim <hello@muniftanjim.dev>
2022-10-30 01:45:38 +0600

    feat(nut): add git.branch

---
+++ lua/nougat/nut/git/branch.lua
@@ -0,0 +1,44 @@
+local Item = require("nougat.item")
+
+local get_content = {
+  fugitive = function()
+    return vim.fn.FugitiveHead()
+  end,
+  gitsigns = function(_, ctx)
+    return vim.b[ctx.bufnr].gitsigns_head
+  end,
+  [""] = function() end,
+}
+
+local mod = {}
+
+function mod.create(opts)
+  local item = Item({
+    hidden = opts.hidden,
+    hl = opts.hl,
+    sep_left = opts.sep_left,
+    prefix = opts.prefix,
+    suffix = opts.suffix,
+    sep_right = opts.sep_right,
+  })
+
+  item.config = vim.tbl_deep_extend("force", {
+    provider = "auto",
+  }, opts.config or {})
+
+  if item.config.provider == "auto" then
+    if pcall(require, "gitsigns") then
+      item.config.provider = "gitsigns"
+    elseif vim.api.nvim_get_runtime_file("plugin/fugitive.vim", false)[1] then
+      item.config.provider = "fugitive"
+    else
+      item.config.provider = ""
+    end
+  end
+
+  item.content = get_content[item.config.provider]
+
+  return item
+end
+
+return mod
