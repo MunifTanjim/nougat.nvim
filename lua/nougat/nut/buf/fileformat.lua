9516912ed4a5e5dc7929f58dac1a3c347dbc4683
Munif Tanjim <hello@muniftanjim.dev>
2022-10-29 14:52:31 +0600

    feat(nut): add buf.fileformat

---
+++ lua/nougat/nut/buf/fileformat.lua
@@ -0,0 +1,29 @@
+local Item = require("nougat.item")
+
+local function get_content(item, ctx)
+  local fileformat = vim.bo[ctx.bufnr].fileformat
+  return item.config.text[fileformat] or fileformat
+end
+
+local mod = {}
+
+function mod.create(opts)
+  local item = Item({
+    hidden = opts.hidden,
+    hl = opts.hl,
+    sep_left = opts.sep_left,
+    prefix = opts.prefix,
+    suffix = opts.suffix,
+    sep_right = opts.sep_right,
+  })
+
+  item.config = vim.tbl_extend("force", {
+    text = {},
+  }, opts.config or {})
+
+  item.content = get_content
+
+  return item
+end
+
+return mod
