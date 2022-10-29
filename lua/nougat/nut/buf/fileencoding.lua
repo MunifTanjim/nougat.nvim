8930674059c5f7a3ca6db0c27c792bce4625acb6
Munif Tanjim <hello@muniftanjim.dev>
2022-10-29 15:01:08 +0600

    feat(nut): add buf.fileencoding

---
+++ lua/nougat/nut/buf/fileencoding.lua
@@ -0,0 +1,36 @@
+local Item = require("nougat.item")
+
+local function get_content(item, ctx)
+  local text = item.config.text
+  return table.concat({
+    vim.bo[ctx.bufnr].fileencoding,
+    vim.bo[ctx.bufnr].bomb and text.bomb or "",
+    vim.bo[ctx.bufnr].endofline and "" or text.noendofline,
+  })
+end
+
+local mod = {}
+
+function mod.create(opts)
+  local item = Item({
+    hidden = opts.hidden,
+    hl = opts.hl,
+    sep_left = opts.sep_left,
+    prefix = opts.prefix,
+    suffix = opts.suffix,
+    sep_right = opts.sep_right,
+  })
+
+  item.config = vim.tbl_extend("force", {
+    text = {
+      bomb = "[BOM]",
+      noendofline = "[!EOL]",
+    },
+  }, opts.config or {})
+
+  item.content = get_content
+
+  return item
+end
+
+return mod
