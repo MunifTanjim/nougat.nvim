199423ea7a0eed0a219b2cf7ad1040d7e5de6bb4
Munif Tanjim <hello@muniftanjim.dev>
2022-12-23 18:58:45 +0600

    feat(bar): add helpers for winbar

--- lua/nougat/bar/util.lua
+++ lua/nougat/bar/util.lua
@@ -3,10 +3,12 @@ local core = require("nui.bar.core")
 local config = {
   statusline = {},
   tabline = {},
+  winbar = {},
 }
 
 local statusline = config.statusline
 local tabline = config.tabline
+local winbar = config.winbar
 
 local statusline_generator = core.generator(function(ctx)
   ctx.width = vim.go.laststatus == 3 and vim.go.columns or vim.api.nvim_win_get_width(ctx.winid)
@@ -50,6 +52,34 @@ end, {
   context = {},
 })
 
+local winbar_generator = core.generator(function(ctx)
+  ctx.width = vim.api.nvim_win_get_width(ctx.winid)
+
+  local select = winbar.select
+
+  return vim.api.nvim_win_call(ctx.winid, function()
+    local wbr = type(select) == "function" and select(ctx) or select
+    return wbr and wbr:generate(ctx) or ""
+  end)
+end, {
+  id = "nougat.go.winbar",
+  context = {},
+})
+
+local winbar_by_filetype_generator = core.generator(function(ctx)
+  ctx.width = vim.api.nvim_win_get_width(ctx.winid)
+
+  local select = winbar.by_filetype[vim.bo[ctx.bufnr].filetype]
+
+  return vim.api.nvim_win_call(ctx.winid, function()
+    local wbr = type(select) == "function" and select(ctx) or select
+    return wbr and wbr:generate(ctx) or ""
+  end)
+end, {
+  id = "nougat.wo.winbar.by_filetype",
+  context = {},
+})
+
 ---@param filetype string
 ---@param bar NougatBar|(fun(ctx:nui_bar_core_expression_context):NougatBar)
 local function set_statusline_for_filetype(filetype, bar)
@@ -76,6 +106,34 @@ local function set_statusline_for_filetype(filetype, bar)
   end
 
   statusline.by_filetype[filetype] = bar
+end
+
+---@param filetype string
+---@param bar NougatBar|(fun(ctx:nui_bar_core_expression_context):NougatBar)
+local function set_winbar_for_filetype(filetype, bar)
+  if not winbar.by_filetype then
+    winbar.by_filetype = {}
+
+    local augroup = vim.api.nvim_create_augroup("nougat.wo.winbar.by_filetype", { clear = true })
+
+    vim.api.nvim_create_autocmd("FileType", {
+      group = augroup,
+      callback = function(info)
+        local bufnr, ft = info.buf, info.match
+        if winbar.by_filetype[ft] then
+          vim.schedule(function()
+            if vim.api.nvim_buf_is_valid(bufnr) then
+              vim.api.nvim_buf_call(bufnr, function()
+                vim.wo.winbar = winbar_by_filetype_generator
+              end)
+            end
+          end)
+        end
+      end,
+    })
+  end
+
+  winbar.by_filetype[filetype] = bar
 end
 
 local mod = {}
@@ -112,6 +170,30 @@ end
 
 function mod.refresh_tabline()
   vim.cmd("redrawtabline")
+end
+
+---@param bar NougatBar|(fun(ctx:nui_bar_core_expression_context):NougatBar)
+---@param opts? { filetype?: string }
+function mod.set_winbar(bar, opts)
+  opts = opts or {}
+
+  if opts.filetype then
+    set_winbar_for_filetype(opts.filetype, bar)
+  else
+    winbar.select = bar
+
+    vim.go.winbar = winbar_generator
+  end
+end
+
+---@param force_all? boolean
+function mod.refresh_winbar(force_all)
+  if force_all then
+    vim.cmd("redrawstatus!")
+    return
+  end
+
+  vim.cmd("redrawstatus")
 end
 
 return mod
