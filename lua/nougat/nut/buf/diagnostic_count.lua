7bf86825a330a6ffb88fcb3c6eb458e032bcc9cd
Munif Tanjim <hello@muniftanjim.dev>
2022-12-29 20:10:02 +0600

    refactor(nut): use shared cache for buf.diagnostic_count

--- lua/nougat/nut/buf/diagnostic_count.lua
+++ lua/nougat/nut/buf/diagnostic_count.lua
@@ -1,20 +1,16 @@
 local Item = require("nougat.item")
 local core = require("nui.bar.core")
-local create_cache_store = require("nougat.cache").create_store
 local u = require("nougat.util")
 
-local severity = vim.deepcopy(vim.diagnostic.severity)
-severity.COMBINED = severity.ERROR + severity.WARN + severity.INFO + severity.HINT
+local diagnostic_cache = require("nougat.cache.diagnostic")
+local severity = diagnostic_cache.severity
 
-local cache_store = create_cache_store("buf", "nut_buf_diagnostic_count", {
-  [severity.ERROR] = 0,
-  [severity.WARN] = 0,
-  [severity.INFO] = 0,
-  [severity.HINT] = 0,
-  [severity.COMBINED] = 0,
-  -- combined content
-  cc = nil,
-})
+diagnostic_cache.on("update", function(cache)
+  -- previous combined content
+  cache.pcc = cache.cc
+  -- invalidate combined content
+  cache.cc = nil
+end)
 
 local function get_count_content(item, ctx)
   local config = item:config(ctx)
@@ -26,13 +22,16 @@ local function get_combined_content(item, ctx)
   local cache = item.cache[ctx.bufnr]
 
   if cache.cc then
+    -- show cached combined content
     return cache.cc
   end
 
+  -- show previous combined content, when current one is cooking
+  cache.cc = cache.pcc
+
   local config = item:config(ctx)
 
-  cache.cc = ""
-
+  -- cook combined content lazily
   vim.schedule(function()
     local part_idx, parts = 0, {}
 
@@ -46,11 +45,11 @@ local function get_combined_content(item, ctx)
         parts[part_idx + 2] = config.sep
         part_idx = part_idx + 2
       end
-      parts[part_idx + 1] = core.highlight(u.set_hl(config.error, item_hl))
-      parts[part_idx + 2] = config.error.prefix
-      parts[part_idx + 3] = cache[severity.ERROR]
-      parts[part_idx + 4] = config.error.suffix
-      part_idx = part_idx + 4
+      part_idx = core.add_highlight(u.set_hl(config.error, item_hl), nil, parts, part_idx)
+      parts[part_idx + 1] = config.error.prefix
+      parts[part_idx + 2] = cache[severity.ERROR]
+      parts[part_idx + 3] = config.error.suffix
+      part_idx = part_idx + 3
     end
 
     if config.warn and cache[severity.WARN] > 0 then
@@ -59,11 +58,11 @@ local function get_combined_content(item, ctx)
         parts[part_idx + 2] = config.sep
         part_idx = part_idx + 2
       end
-      parts[part_idx + 1] = core.highlight(u.set_hl(config.warn, item_hl))
-      parts[part_idx + 2] = config.warn.prefix
-      parts[part_idx + 3] = cache[severity.WARN]
-      parts[part_idx + 4] = config.warn.suffix
-      part_idx = part_idx + 4
+      part_idx = core.add_highlight(u.set_hl(config.warn, item_hl), nil, parts, part_idx)
+      parts[part_idx + 1] = config.warn.prefix
+      parts[part_idx + 2] = cache[severity.WARN]
+      parts[part_idx + 3] = config.warn.suffix
+      part_idx = part_idx + 3
     end
 
     if config.info and cache[severity.INFO] > 0 then
@@ -72,11 +71,11 @@ local function get_combined_content(item, ctx)
         parts[part_idx + 2] = config.sep
         part_idx = part_idx + 2
       end
-      parts[part_idx + 1] = core.highlight(u.set_hl(config.info, item_hl))
-      parts[part_idx + 2] = config.info.prefix
-      parts[part_idx + 3] = cache[severity.INFO]
-      parts[part_idx + 4] = config.info.suffix
-      part_idx = part_idx + 4
+      part_idx = core.add_highlight(u.set_hl(config.info, item_hl), nil, parts, part_idx)
+      parts[part_idx + 1] = config.info.prefix
+      parts[part_idx + 2] = cache[severity.INFO]
+      parts[part_idx + 3] = config.info.suffix
+      part_idx = part_idx + 3
     end
 
     if config.hint and cache[severity.HINT] > 0 then
@@ -85,61 +84,19 @@ local function get_combined_content(item, ctx)
         parts[part_idx + 2] = config.sep
         part_idx = part_idx + 2
       end
-      parts[part_idx + 1] = core.highlight(u.set_hl(config.hint, item_hl))
-      parts[part_idx + 2] = config.hint.prefix
-      parts[part_idx + 3] = cache[severity.HINT]
-      parts[part_idx + 4] = config.hint.suffix
+      part_idx = core.add_highlight(u.set_hl(config.hint, item_hl), nil, parts, part_idx)
+      parts[part_idx + 1] = config.hint.prefix
+      parts[part_idx + 2] = cache[severity.HINT]
+      parts[part_idx + 3] = config.hint.suffix
+      part_idx = part_idx + 3
     end
 
-    cache.cc = table.concat(parts)
+    cache.cc = table.concat(parts, nil, 1, part_idx)
+    cache.pcc = nil
   end)
 
   return cache.cc
 end
-
-local augroup = vim.api.nvim_create_augroup("nougat.nut.buf.diagnostic_count", { clear = true })
-
-vim.api.nvim_create_autocmd("DiagnosticChanged", {
-  group = augroup,
-  callback = function(params)
-    local error, warn, info, hint = 0, 0, 0, 0
-
-    local diagnostics = vim.diagnostic.get(params.buf)
-
-    for idx = 1, #diagnostics do
-      local diagnostic = diagnostics[idx]
-      if diagnostic.severity == severity.ERROR then
-        error = error + 1
-      elseif diagnostic.severity == severity.WARN then
-        warn = warn + 1
-      elseif diagnostic.severity == severity.INFO then
-        info = info + 1
-      elseif diagnostic.severity == severity.HINT then
-        hint = hint + 1
-      end
-    end
-
-    local cache = cache_store[params.buf]
-    if cache[severity.ERROR] ~= error then
-      cache[severity.ERROR] = error
-      cache.cc = nil
-    end
-    if cache[severity.WARN] ~= warn then
-      cache[severity.WARN] = warn
-      cache.cc = nil
-    end
-    if cache[severity.INFO] ~= info then
-      cache[severity.INFO] = info
-      cache.cc = nil
-    end
-    if cache[severity.HINT] ~= hint then
-      cache[severity.HINT] = hint
-      cache.cc = nil
-    end
-    cache[severity.COMBINED] = error + warn + info + hint
-  end,
-  desc = "[nougat] nut.buf.diagnostic_count",
-})
 
 local mod = {}
 
@@ -175,7 +132,7 @@ function mod.create(opts)
     context = opts.context,
   })
 
-  item.cache = cache_store
+  item.cache = diagnostic_cache.store
 
   return item
 end
