e3d3d60609e4ecfec805ccc3cf29b67f6767f067
Munif Tanjim <hello@muniftanjim.dev>
2022-12-29 21:06:36 +0600

    feat(nut): diagnostic hl for tab.tablist.label

--- lua/nougat/nut/tab/tablist/label.lua
+++ lua/nougat/nut/tab/tablist/label.lua
@@ -10,7 +10,49 @@ local function get_content(_, ctx)
   return core.label(vim.fn.fnamemodify(vim.api.nvim_buf_get_name(tab_ctx.bufnr), ":t"), o_label_opts)
 end
 
-local mod = {}
+local hl = {}
+
+local diagnostic_cache, diagnostic_severity, diagnostic_hl_group_by_severity
+
+local function hl_diagnostic(_, ctx)
+  return diagnostic_hl_group_by_severity[diagnostic_cache[ctx.ctx.tab.bufnr].max]
+end
+
+function hl.diagnostic()
+  if not diagnostic_cache then
+    do
+      local ncd = require("nougat.cache.diagnostic")
+      ncd.on("update", function(cache)
+        if cache[diagnostic_severity.ERROR] > 0 then
+          cache.max = diagnostic_severity.ERROR
+        elseif cache[diagnostic_severity.WARN] > 0 then
+          cache.max = diagnostic_severity.WARN
+        elseif cache[diagnostic_severity.INFO] > 0 then
+          cache.max = diagnostic_severity.INFO
+        elseif cache[diagnostic_severity.HINT] > 0 then
+          cache.max = diagnostic_severity.HINT
+        else
+          cache.max = 0
+        end
+      end)
+
+      diagnostic_cache = ncd.store
+      diagnostic_severity = ncd.severity
+      diagnostic_hl_group_by_severity = {
+        [diagnostic_severity.ERROR] = "DiagnosticError",
+        [diagnostic_severity.WARN] = "DiagnosticWarn",
+        [diagnostic_severity.INFO] = "DiagnosticInfo",
+        [diagnostic_severity.HINT] = "DiagnosticHint",
+      }
+    end
+  end
+
+  return hl_diagnostic
+end
+
+local mod = {
+  hl = hl,
+}
 
 function mod.create(opts)
   local item = Item({
